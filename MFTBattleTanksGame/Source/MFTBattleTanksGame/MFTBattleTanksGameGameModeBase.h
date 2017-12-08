54691911e4c395e9a2ca629d7c63037385b9e1d6
WORGENORC <t.choki@abv.bg>
2017-12-09 00:32:10 +0200

    Mai pochvam da razbiram durvoto S4 L111

---
+++ MFTBattleTanksGame/Source/MFTBattleTanksGame/MFTBattleTanksGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MFTBattleTanksGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MFTBATTLETANKSGAME_API AMFTBattleTanksGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
