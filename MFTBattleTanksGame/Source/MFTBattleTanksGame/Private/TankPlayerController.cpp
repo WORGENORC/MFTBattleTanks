e38b8d19f0895b36650dfddcec4ab0fbe20e22e9
WORGENORC <t.choki@abv.bg>
2017-12-12 19:29:58 +0200

    S4 L127

--- MFTBattleTanksGame/Source/MFTBattleTanksGame/Private/TankPlayerController.cpp
+++ MFTBattleTanksGame/Source/MFTBattleTanksGame/Private/TankPlayerController.cpp
@@ -17,9 +17,23 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
-ATank * ATankPlayerController::GetControlledTank() const
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	// AimTowardsCrosshair();
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location if linetrace through crosshair
+	// if it hits the landscape
+		// Tell controlled tank to aim at this point 
+}
 
